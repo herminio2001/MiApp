0d981d812a991f6d1d193bb0d8b42a462de4652b
herminio2001 <herminiomartinezdionisio@gmail.com>
2022-04-11 18:03:42 +0000

    V14 recuperar

--- mvc/controllers/public/recuperar_con.py
+++ mvc/controllers/public/recuperar_con.py
@@ -3,9 +3,18 @@ import app
 import pyrebase
 import firebase_config as token
 import json  
-        
+
+
 render = web.template.render("mvc/view/public", base="layout")
            
 class Recuperar_con:
     def GET(self):
         return render.recuperar_con()
+
+    def POST(self):
+        firebase = pyrebase.initialize_app(token.firebaseConfig)
+        auth = firebase.auth()
+        formulario = web.input()
+        email = formulario.email
+        user = auth.send_password_reset_email(email)
+        return render.recuperar_con() 
