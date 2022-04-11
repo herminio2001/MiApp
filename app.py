1953a3ff439d6640dafd2d6c2784bf0e9b31a318
herminio2001 <herminiomartinezdionisio@gmail.com>
2022-04-11 00:03:56 +0000

    V8 url

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import web
 
 urls = (
     '/login', 'mvc.controllers.public.login.Login',
-    '/index', 'mvc.controllers.public.index.Index', 
-    #'/list_user', 'mvc.controllers.public.list.List_user'
+    '/index', 'mvc.controllers.public.index.Index',
+    '/list_user', 'mvc.controllers.public.list_user.List_user',
+    '/registrar', 'mvc.controllers.public.registrar.Registrar',
+    '/principal', 'mvc.controllers.public.principal.Principal',
+    '/dash', 'mvc.controllers.public.dash.Dash',
+    '/update', 'mvc.controllers.public.update.Update',
+    '/login_ope', 'mvc.controllers.public.login_ope.Login_ope',
+    '/principal_ope', 'mvc.controllers.public.principal_ope.Principal_ope',
 )       
  
 app = web.application(urls, globals())  
