a790f7215786ecb9743d8ac874708e5a9d4d0ef4
herminio2001 <herminiomartinezdionisio@gmail.com>
2022-04-12 02:38:46 +0000

    V14

--- mvc/controllers/public/list_user.py
+++ mvc/controllers/public/list_user.py
@@ -5,7 +5,19 @@ import firebase_config as token
 import json  
         
 render = web.template.render("mvc/view/public", base="layout")
+firebase = pyrebase.initialize_app(token.firebaseConfig)
+auth = firebase.auth()
+db = firebase.database()  
            
 class List_user:
     def GET(self):
-        return render.list_user() 
+        #return render.list_user()  
+        datos_usuarios = db.child("users").get()
+        print(datos_usuarios)
+        return render.list_user(datos_usuarios)  
+        try:
+            datos_usuarios = db.child("users").get()
+            print(datos_usuarios)
+            return render.list_user(datos_usuarios) 
+        except Exception as error:
+            return render.list_user(error) 
